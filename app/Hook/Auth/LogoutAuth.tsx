ceaab5897485945b3f2aa8934500eb2d0da89ffc
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-07-23 19:18:25 +0300

    Update LogoutAuth.tsx

--- app/Hook/Auth/LogoutAuth.tsx
+++ app/Hook/Auth/LogoutAuth.tsx
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 export const LogoutAuth = () => {
   const queryClient = useQueryClient();
@@ -9,13 +10,16 @@ export const LogoutAuth = () => {
   const { mutate: logout } = useMutation({
     mutationFn: async () => {
       try {
-        const res = await fetch("https://twitter-backend-mauve.vercel.app/api/auth/logout", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        });
+        const res = await fetch(
+          "https://twitter-backend-mauve.vercel.app/api/auth/logout",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          }
+        );
         const data = await res.json();
         if (res.ok) {
           router.push("/"); // إعادة توجيه المستخدم إلى صفحة تسجيل الدخول
@@ -31,8 +35,7 @@ export const LogoutAuth = () => {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["authUser"] });
-      // queryClient.removeQueries({ queryKey: ["authUser"] });
-
+      Cookies.remove("Succes");
       toast.success("Logout successfully");
     },
     onError: () => {
