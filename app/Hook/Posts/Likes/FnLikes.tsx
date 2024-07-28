db11ef11163006d2ea940b45b758198027d156ca
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-07-28 18:06:17 +0300

    Update FnLikes.tsx

--- app/Hook/Posts/Likes/FnLikes.tsx
+++ app/Hook/Posts/Likes/FnLikes.tsx
@@ -30,15 +30,7 @@ export const FnLikeProcess = ({ post }: any) => {
       }
     },
     onSuccess: (updatedLikes) => {
-      // instead, update the cache directly for that post
-      queryClient.setQueryData(["posts"], (oldData: any) => {
-        return oldData.map((p: any) => {
-          if (p._id === post._id) {
-            return { ...p, likes: updatedLikes };
-          }
-          return p;
-        });
-      });
+     
       queryClient.invalidateQueries({ queryKey: ["posts"] });
     },
     onError: (error) => {
