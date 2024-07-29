689633510ecc6ba28b46b2a2afce18223f4288e3
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-07-30 02:19:29 +0300

    Add files via upload

---
+++ app/Hook/Posts/AutoUp/FnUpdate.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+export const FnUpdatedPost = (ID: any) => {
+  const { data: updatedPost, refetch } = useQuery({
+    queryKey: ["post", ID],
+    queryFn: async () => {
+      const token = localStorage.getItem("jwt");
+      const res = await fetch(`http://localhost:5005/api/posts/${ID}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        credentials: "include",
+      });
+
+      const data = await res.json();
+      return data;
+    },
+    refetchInterval: 10000, // جلب البيانات كل 10 ثواني
+  });
+
+  return { updatedPost };
+};
