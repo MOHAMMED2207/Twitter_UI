a833259bbd67b425c91dc6dfed1773d076712e97
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-07-31 17:47:34 +0300

    Add files via upload

---
+++ app/Hook/Posts/UniqePosts/FnUniqePosts.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export const FnUniqePosts = ({ setPost, id }: any) => {
+  const router = useRouter();
+  const { data, isLoading, refetch, isRefetching } = useQuery({
+    queryKey: ["Pk_Posts"],
+    queryFn: async () => {
+      try {
+        const token = localStorage.getItem("jwt");
+        const res = await fetch(`http://localhost:5005/api/posts/${id}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          credentials: "include",
+        });
+
+        if (!res.ok) {
+          throw new Error("Post not found");
+        }
+
+        if (!res) {
+          throw new Error("Post not found");
+        }
+        const data = await res.json();
+        setPost(data);
+      } catch (error: any) {
+        console.error("Error fetching post:", error);
+        router.back;
+      }
+    },
+  });
+  useEffect(() => {
+    refetch();
+  }, [id]);
+
+  return {
+    isLoading,
+    refetch,
+    isRefetching,
+  };
+};
