181b91699c3e0f742587f4ed44e7abfdf842425b
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-08-02 03:12:23 +0300

    Update GetAllPosts.tsx

--- app/Hook/Posts/GetPostesAllFeedTypes/GetAllPosts.tsx
+++ app/Hook/Posts/GetPostesAllFeedTypes/GetAllPosts.tsx
@@ -2,8 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
 export const GetAllPosts = ({ feedType, username, userId, page }: any) => {
-  
-const getPostEndpoint = () => {
+  const getPostEndpoint = () => {
     switch (feedType) {
       case "forYou":
         return `https://twitter-backend-mauve.vercel.app/api/post/all?page=${page}&limit=10`;
@@ -11,13 +10,14 @@ const getPostEndpoint = () => {
         return `https://twitter-backend-mauve.vercel.app/api/post/following?page=${page}&limit=10`;
       case "post":
         return `https://twitter-backend-mauve.vercel.app/api/post/user/${username}?page=${page}&limit=10`;
+      case "SavePost":
+        return `https://twitter-backend-mauve.vercel.app/api/post/Save/${username}?page=${page}&limit=10`;
       case "likes":
         return `https://twitter-backend-mauve.vercel.app/api/post/likes/${username}?page=${page}&limit=10`;
       default:
         return `https://twitter-backend-mauve.vercel.app/api/post/all?page=${page}&limit=10`;
     }
   };
-
   const POST_ENDPOINT = getPostEndpoint();
 
   const { data, isLoading, refetch, isRefetching } = useQuery({
@@ -34,20 +34,19 @@ const getPostEndpoint = () => {
           },
           credentials: "include",
         });
-        const data = await res.json();
 
+        const data = await res.json();
         return data;
       } catch (error: any) {
         throw new Error(error);
       }
     },
-    // keepPreviousData: true,
+    refetchInterval: 3600000, // 1 hour
   });
 
   useEffect(() => {
     refetch();
   }, [feedType, refetch, username, page]);
-
   return {
     posts: data?.posts,
     totalPosts: data?.totalPosts,
